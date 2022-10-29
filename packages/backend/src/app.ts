b0d37b64edc6efd1256b3ea05e2a664f626ef0c2
Will S <will@sonnex.name>
2022-10-29 15:24:00 -0700

    Ran formatter, and did misc cleanup

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
+import express from "express";
+import { NestFactory } from "@nestjs/core";
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
 
 export async function createNestApp() {
   const app = express();
   const nestApp = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new ExpressAdapter(app),
+    new ExpressAdapter(app)
   );
   await nestApp.init();
   return app;
