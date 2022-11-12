89d53d730d852db5c7aee5c12ef894cd028e1377
Will S <will@sonnex.name>
2022-11-11 17:42:41 -0800

    Setting audience on auth request

--- packages/frontend/src/components/AuthProvider/AuthProvider.tsx
+++ packages/frontend/src/components/AuthProvider/AuthProvider.tsx
@@ -4,14 +4,16 @@ type AuthProviderProps = React.PropsWithChildren;
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENTID;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId) {
+  if (!domain || !clientId || !audience) {
     throw new Error("Authorization settings missing");
   }
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
+      audience={audience}
       redirectUri={window.location.origin}
     >
       {children}
