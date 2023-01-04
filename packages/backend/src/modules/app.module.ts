fef791ef99519082a4e7b0f4107076bd77e38fc9
Will S <will.sonnex@ca.ey.com>
2023-01-04 10:46:36 -0800

    before nuking yarn.lock to try and fix issues

--- packages/backend/src/modules/app.module.ts
+++ packages/backend/src/modules/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { DbModule } from "./db/db.module";
-import { OpportunityController } from "./opportunity/opportunity.controller";
 import { OpportunityModule } from "./opportunity/opportunity.module";
 import { RootController } from "./root/root.controller";
 
