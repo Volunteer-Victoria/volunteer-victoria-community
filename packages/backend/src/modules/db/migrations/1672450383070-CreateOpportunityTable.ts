6a7d09a7e951bf0e5ee00b7c57ed5dadc2ffd6ca
Will S <will.sonnex@ca.ey.com>
2023-01-04 14:37:37 -0800

    Missized columns in db

--- packages/backend/src/modules/db/migrations/1672450383070-CreateOpportunityTable.ts
+++ packages/backend/src/modules/db/migrations/1672450383070-CreateOpportunityTable.ts
@@ -9,18 +9,18 @@ export class CreateOpportunityTable1672450383070 implements MigrationInterface {
           "title"                       text NOT NULL,
           "contactName"                 text NOT NULL,
           "requiredPeopleCount"         int NOT NULL,
-          "occursDate"                  char(10) NOT NULL,
+          "occursDate"                  text NOT NULL,
           "occursTime"                  text,
           "description"                 text NOT NULL,
           "locationName"                text NOT NULL,
-          "indoorsOrOutdoors"           varchar(8) NOT NULL,
+          "indoorsOrOutdoors"           text NOT NULL,
           "contactEmail"                text,
           "contactPhone"                text,
           "criminalRecordCheckRequired" boolean NOT NULL,
           "idealVolunteer"              text,
           "additionalInformation"       text,
           "postedTime"                  bigint,
-          "postedByUserId"              char(21),
+          "postedByUserId"              text,
           
           PRIMARY KEY ("opportunityId")
       );
