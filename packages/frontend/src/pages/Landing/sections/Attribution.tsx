ed5cf103f03bfb32c6fc5755f3120150caf95691
Josh Stabback <josh.stabback@gmail.com>
2023-03-21 08:33:49 -0700

    feat: add attribution

---
+++ packages/frontend/src/pages/Landing/sections/Attribution.tsx
@@ -0,0 +1,42 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import { Section } from "./Section";
+import GovCanada from "../../../assets/gov-canada.png";
+
+export const AttributionSection = () => {
+  return (
+    <Section background="white">
+      <Typography
+        variant="h2"
+        color="primary"
+        textAlign="center"
+        mb={{ xs: 2, lg: 10 }}
+      >
+        Attribution
+      </Typography>
+      <Grid container direction="row" spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="subtitle2" gutterBottom>
+            We are deeply thankful for the guidance, help and funding we’ve
+            received to build a supportive and accessible community for our
+            seniors in Victoria.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          alignItems="center"
+          justifyItems="center"
+          display="flex"
+        >
+          <img
+            src={GovCanada}
+            alt="Funded by the government of Canada New Horizons for Seniors Program"
+            loading="lazy"
+          />
+        </Grid>
+      </Grid>
+    </Section>
+  );
+};
